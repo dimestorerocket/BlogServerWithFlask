4d308626b66701a075b17bbee3b3a45cd3371e9a
dimestorerocket <dimestorerocket@gmail.com>
2020-06-22 04:55:49 -0500

    Auto Link Creation.

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -5,8 +5,8 @@ from flask_blog import app
 @app.route('/')
 def show_entries():
     if not session.get('logged_in'):
-        return redirect('/login')
-    return render_template('/index.html')
+        return redirect(url_for('login'))
+    return render_template('entries/index.html')
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -19,11 +19,11 @@ def login():
         else:
             session['logged_in'] = True
             flash('Login Successful')
-            return redirect('/')
-    return render_template('/login.html')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html')
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('Logged Out')
-    return redirect('/')
+    return redirect(url_for('show_entries'))
